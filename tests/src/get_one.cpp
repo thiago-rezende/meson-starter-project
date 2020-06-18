8c064ea72c7888b694373f7bc3a6d7ecbd87a53a
Thiago Rezende <bitrateti@gmail.com>
2020-06-18 19:35:21 -0300

    docs: added docstring to get_one test

--- tests/src/get_one.cpp
+++ tests/src/get_one.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file get_one.cpp
+ * @author Thiago Rezende (bitrateti@gmail.com)
+ * @brief Test Case for horus::get_one
+ * @version 0.1
+ * @date 2020-06-18
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
 #include <stdio.h>
 
 #include "horus.h"
