fbaab691b2f43ab857dee66e6169012ce4118cf2
Thiago Rezende <bitrateti@gmail.com>
2020-06-18 19:03:00 -0300

    feat(horus): added libhorus files

---
+++ horus/include/horus.h
@@ -0,0 +1,11 @@
+#ifndef HORUS_H
+#define HORUS_H
+
+#include <iostream>
+
+namespace horus
+{
+    void greet(const char *name);
+} // namespace horus
+
+#endif // HORUS_H
---
+++ horus/src/horus.cpp
@@ -0,0 +1,9 @@
+#include "horus.h"
+
+namespace horus
+{
+    void greet(const char *name)
+    {
+        std::cout << "[HORUS]: Hello " << name << std::endl;
+    }
+} // namespace horus
