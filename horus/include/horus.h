7fee2168066e7833cfbc9a20afc4e91e683ddaf1
Thiago Rezende <bitrateti@gmail.com>
2020-06-18 19:06:17 -0300

    docs(horus): added docstrings to horus.h

--- horus/include/horus.h
+++ horus/include/horus.h
@@ -1,10 +1,30 @@
+/**
+ * @file horus.h
+ * @author Thiago Rezende (bitrateti@gmail.com)
+ * @brief Horus Lib header file
+ * @version 0.1
+ * @date 2020-06-18
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
 #ifndef HORUS_H
 #define HORUS_H
 
 #include <iostream>
 
+/**
+ * @brief Horus Lib namespace
+ *
+ */
 namespace horus
 {
+    /**
+     * @brief Greets a given name
+     *
+     * @param name Name to greet
+     */
     void greet(const char *name);
 } // namespace horus
 
--- horus/src/horus.cpp
+++ horus/src/horus.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file horus.h
+ * @author Thiago Rezende (bitrateti@gmail.com)
+ * @brief Horus Lib source file
+ * @version 0.1
+ * @date 2020-06-18
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
 #include "horus.h"
 
 namespace horus
